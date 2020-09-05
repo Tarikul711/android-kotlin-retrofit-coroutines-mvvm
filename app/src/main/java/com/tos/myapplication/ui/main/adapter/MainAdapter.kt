045277c3ab4542a765cc7d5edb7da5e8bd964711
Tarikul711 <Tarikul711@gmail.com>
2020-09-05 11:47:03 +0600

    create a user adapter

---
+++ app/src/main/java/com/tos/myapplication/ui/main/adapter/MainAdapter.kt
@@ -0,0 +1,55 @@
+package com.tos.myapplication.ui.main.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.tos.myapplication.R
+import com.tos.myapplication.data.model.User
+import kotlinx.android.synthetic.main.item_layout.view.*
+
+/**
+ *Created by tarikul on 5/9/20
+ */
+class MainAdapter(private val users: ArrayList<User>) :
+    RecyclerView.Adapter<MainAdapter.DataViewHolder>() {
+    class DataViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(user: User) {
+            itemView.apply {
+                textViewUserName.text = user.name
+                textViewUserEmail.text = user.email
+                Glide.with(imageViewAvatar.context)
+                    .load(user.avatar)
+                    .into(imageViewAvatar)
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataViewHolder =
+        DataViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_layout,
+                parent,
+                false
+            )
+        )
+
+    override fun getItemCount(): Int {
+        return users.size
+    }
+
+    override fun onBindViewHolder(holder: DataViewHolder, position: Int) {
+        holder.bind(users[position])
+    }
+
+    fun addUsers(users: List<User>) {
+        this.users.apply {
+            clear()
+            addAll(users)
+        }
+    }
+}
+
+
