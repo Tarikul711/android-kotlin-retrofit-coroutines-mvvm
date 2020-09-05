1c46396586f038de6e766b2ecb4cda5bbf70a073
Tarikul711 <Tarikul711@gmail.com>
2020-09-05 10:59:52 +0600

    add logger interceptor in retrofit

--- app/src/main/java/com/tos/myapplication/data/api/RetrofitBuilder.kt
+++ app/src/main/java/com/tos/myapplication/data/api/RetrofitBuilder.kt
@@ -1,5 +1,8 @@
 package com.tos.myapplication.data.api
 
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -8,12 +11,23 @@ import retrofit2.converter.gson.GsonConverterFactory
  */
 object RetrofitBuilder {
     private const val BASE_URL = "https://5e510330f2c0d300147c034c.mockapi.io/"
+
+
+    var httpClient: OkHttpClient.Builder = OkHttpClient.Builder().addInterceptor(interceptor())
+
     private fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(httpClient.build())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
 
     val apiService: ApiService = getRetrofit().create(ApiService::class.java)
+
+    private fun interceptor(): HttpLoggingInterceptor {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        return httpLoggingInterceptor
+    }
 }
