880adca8b6fce6ded63337fc8950d37f12601909
Tarikul711 <Tarikul711@gmail.com>
2020-09-05 12:24:42 +0600

    add viewmodelfactory and MainViewModel class

---
+++ app/src/main/java/com/tos/myapplication/ui/base/ViewModelFactory.kt
@@ -0,0 +1,20 @@
+package com.tos.myapplication.ui.base
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.tos.myapplication.data.api.ApiHelper
+import com.tos.myapplication.data.repository.MainRepository
+import com.tos.myapplication.ui.main.viewmodel.MainViewModel
+import java.lang.IllegalArgumentException
+
+/**
+ *Created by tarikul on 5/9/20
+ */
+class ViewModelFactory(val apiHelper: ApiHelper) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            return MainViewModel(MainRepository(apiHelper)) as T
+        }
+        throw IllegalArgumentException("Unknown class name")
+    }
+}
---
+++ app/src/main/java/com/tos/myapplication/ui/main/viewmodel/MainViewModel.kt
@@ -0,0 +1,22 @@
+package com.tos.myapplication.ui.main.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.tos.myapplication.data.repository.MainRepository
+import com.tos.myapplication.utils.Resource
+import kotlinx.coroutines.Dispatchers
+
+/**
+ *Created by tarikul on 5/9/20
+ */
+class MainViewModel(val mainRepository: MainRepository) : ViewModel() {
+
+    fun getUsers() = liveData(Dispatchers.IO) {
+        emit(Resource.loading(data = null))
+        try {
+            emit(Resource.success(data = mainRepository.getUsers()))
+        } catch (exception: Exception) {
+            emit(Resource.error(data = null, message = exception.message ?: "Error Occurred!"))
+        }
+    }
+}
